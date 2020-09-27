6fbcee999cbe42f6ab100c0c25c33222014a7780
Mothskee <54814906+Mothskee@users.noreply.github.com>
2020-09-27 15:31:39 -0600

    Scores and Animal Teleports

--- Assets/Scripts/State Machine/KeepScore.cs
+++ Assets/Scripts/State Machine/KeepScore.cs
@@ -5,19 +5,25 @@ using UnityEngine;
 public class KeepScore : MonoBehaviour
 {
     public static int Score = 0;
+
+    //var rand = Random.Range(-4.5f, 4.5f);
     // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter(Collider other)
     {
+        if (other.gameObject.tag == "Animal")
+        {
+            Debug.Log("Animal Entered");  
+
+            Score++;
+
+            other.transform.position = new Vector3(15, transform.position.y, transform.position.z);
+
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     void OnGUI()
     {
-        GUI.Box(new Rect(100, 100, 100, 100),"Score: " + Score.ToString() );
+        GUI.Box(new Rect(100, 100, 100, 40),"Score: " + Score.ToString() );
     }
 }
